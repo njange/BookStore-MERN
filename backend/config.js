4af361ef4556c82631d7e760e35e60a8521228fc
njange <jamesnjange80@gmail.com>
2024-05-11 23:17:34 +0300

    Add MongoDB and moongose to node js and established database connection

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5000;
+
+export const mongoDBURL = 'mongodb+srv://jamesnjange80:root@bookstoremern.oqwnxpi.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStoreMERN'
