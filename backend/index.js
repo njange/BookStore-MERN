fdc4b3ffd2db348b84c895d58f2d042a74e092c6
njange <jamesnjange80@gmail.com>
2024-05-11 21:44:54 +0300

    First HTTP Route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
 });
