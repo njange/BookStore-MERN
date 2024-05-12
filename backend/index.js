25128f640b21b1d2c427e09d37e2d3a3cd7ff626
njange <jamesnjange80@gmail.com>
2024-05-12 23:04:32 +0300

    route to create a new book

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,31 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js'
 
 const app = express();
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
+// Route to saving a new book
+app.post('/books', async (request, response) => {
+    try{
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+    }
+}catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 });
 
 mongoose
