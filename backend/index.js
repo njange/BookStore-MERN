f4e410e137f73a8471e0924f587c2c350c265133
njange <jamesnjange80@gmail.com>
2024-05-12 23:07:44 +0300

    created new book proceeding to testing using postman

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,11 @@ app.post('/books', async (request, response) => {
             return response.status(400).send({
                 message: 'Send all required fields: title, author, publishYear',
             });
-    }
+    };
+    const book = await Book.create(newBook);
+    
+    return response.status(201).send(book);
+
 }catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
