258db96594bc7118f92df39ced5826484ab231fe
njange <jamesnjange80@gmail.com>
2024-05-14 18:01:21 +0300

    multipage routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,11 @@ import ShowBook from './pages/ShowBook';
 const App = () => {
   return (
     <Routes>
-      <Route path= '' element= {} />
-      <Route path= '' element= {} />
-      <Route path= '' element= {} />
-      <Route path= '' element= {} />
-      <Route path= '' element= {} />
+      <Route path= '/' element= {<Home/>} />
+      <Route path= '/books/create' element= {<CreateBook/>} />
+      <Route path= '/book/details:id' element= {<ShowBook />} />
+      <Route path= '/books/edit/:id' element= {<EditBook/>} />
+      <Route path= '/books/delete/:id' element= {<DeleteBook/>} />
     </Routes>
   );
 };
