7fd12be776b37460f1e25f08f1d82d5df3d794f6
njange <jamesnjange80@gmail.com>
2024-05-14 18:05:19 +0300

    route fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes, Router} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import CreateBook from './pages/CreateBook';
 import DeleteBook from './pages/DeleteBook';
