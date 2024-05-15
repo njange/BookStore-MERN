d444c73e1cea4f5d06355bf27bef36c59fc2b258
njange <jamesnjange80@gmail.com>
2024-05-15 13:16:39 +0300

    </th> elements

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -38,6 +38,10 @@ const Home = () => {
                 <thead>
                     <tr>
                         <th className='boarder boarder-slate-600 rounded-md'>No</th>
+                        <th className='border border-slate-600 rounded-md'>Title</th>
+                        <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
+                        <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
+                        <th className='border border-slate-600 rounded-md'>Operations</th>
                     </tr>
                 </thead>
             </table>
