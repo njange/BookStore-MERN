f1ccd18f61418d998de2c72ce6c617f45b16dd78
njange <jamesnjange80@gmail.com>
2024-05-17 01:35:10 +0300

    spinner elements

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -35,6 +35,19 @@ const ShowBook = () => {
         <div className= 'flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
             <div className='my-4'>
                 <span className='text-xl mr-4 text-gray-500'>Id</span>
+                <span>{book._id}</span>
+            </div>
+            <div className='my-4'>
+                <span className='text-xl mr-4 text-gray-500'>Title</span>
+                <span>{book.title}</span>
+            </div>
+            <div className='my-4'>
+                <span className='text-xl mr-4 text-gray-500'>Author</span>
+                <span>{book.publishYear}</span>
+            </div>
+            <div className='my-4'>
+                <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
+                <span>{book._id}</span>
             </div>
         </div>
     )}
