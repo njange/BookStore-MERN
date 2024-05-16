157c8d897047e24ac1f1a2bebe7b9b7a42336285
njange <jamesnjange80@gmail.com>
2024-05-16 14:44:26 +0300

    showbook  retrun function

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import BackButon from '../components/BackButon';
+import BackButton from '../components/BackButon';
 import Spinner from '../components/Spinner';
 
 
@@ -26,7 +26,19 @@ const ShowBook = () => {
 
 
   return (
-    <div></div>
+    <div className='p-4'>
+        <BackButton />
+        <h1 className='text-3xl my-4'>Show Book</h1>
+        {loading ? 
+        <Spinner />
+    ) : (
+        <div className= 'flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
+            <div className='my-4'>
+                <span className='text-xl mr-4 text-gray-500'>Id</span>
+            </div>
+        </div>
+    )}
+    </div>
   )
 }
 
