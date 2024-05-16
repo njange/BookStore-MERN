321d9cce6db60beb46222db65b46d19553691308
njange <jamesnjange80@gmail.com>
2024-05-16 14:31:53 +0300

    backbutton component

--- frontend/src/components/BackButon.jsx
+++ frontend/src/components/BackButon.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { BsArrowLeft} from 'react-icons/bs'
 
-const BackButon = () => {
+const BackButon = ({ destination = '/' }) => {
   return (
-    <div>BackButon</div>
+    <div className='flex'>
+        <Link
+        to={destination}
+        className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
+        >
+            <BsArrowLeft className='text-2xl' />
+        </Link>
+    </div>
   )
 }
 
--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import BackButon from '../components/BackButon';
+import Spinner from '../components/Spinner';
+
 
 const ShowBook = () => {
+    const [book, setBook] =useState({});
+    const [loading, setLoading] = useState(false);
+    const { id } = useParams();
+    useEffect(() =>{
+        setLoading(true);
+        axios.get(`http://localhost:5000/books/${id}`)
+
+    }, [])
+
+    
   return (
     <div>ShowBook</div>
   )
