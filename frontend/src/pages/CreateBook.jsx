53145a558904ec89788eb39e17b8be5af563b3a4
njange <jamesnjange80@gmail.com>
2024-05-17 03:59:18 +0300

    Create abook in react

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -62,6 +62,9 @@ const CreateBook = () => {
           className='border-2 border-gray-500 px-4 py-2 w-full'
           />
         </div>
+        <button classname='p-2 bg-sky-300 m-8' onClick={(handleSaveBook)}>
+          Save
+        </button>
       </div>
     </div>
   )
