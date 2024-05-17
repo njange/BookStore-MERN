318c59e9b84c2267dabb7459acbe6a282f04398d
njange <jamesnjange80@gmail.com>
2024-05-17 05:11:49 +0300

    delete book button

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -25,7 +25,18 @@ const DeleteBook = () => {
     <div className='p-4'>
       <BackButton />
       <h1 className='text-3xl my-4'>DeleteBook</h1>
-      {loading ? : }
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
+        <h3 className='text-2xl'>Are You Sure You Want to delete this book?</h3>
+
+        <button 
+          className='p-4 bg-red-600 text-white m-8 w-full'
+          onClick={handDeleteBook}
+          >
+            Yes,Delete it
+
+        </button>
+      </div>
       </div>
   )
 }
